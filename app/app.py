89ceaad5cb0a47264b3b5dd6011c63e347e3d42d
Utkarsh Gupta <utkarsh348@gmail.com>
2022-03-28 14:07:35 +0530

    mongo express and server configured

--- app/app.py
+++ app/app.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-user = ''           # username as set for the mongodb admin server (the username used in secret.yaml - before base64 conversion)
-password = ''       # password as set for the mongodb admin server (the password used in secret.yaml - before base64 conversion)
-host = ''    # service name of the mongodb admin server as set in mongo-deployment.yaml
-port = ''              # port number of the mongodb admin server as set in mongo-deployment.yaml
+user = 'user1'           # username as set for the mongodb admin server (the username used in secret.yaml - before base64 conversion)
+password = 'user1'       # password as set for the mongodb admin server (the password used in secret.yaml - before base64 conversion)
+host = 'mongi-express-service'    # service name of the mongodb admin server as set in mongo-deployment.yaml
+port = '8081'              # port number of the mongodb admin server as set in mongo-deployment.yaml
 conn_string = f'mongodb://{user}:{password}@{host}:{port}'
 
 conn_string = f'mongodb://127.0.0.1:27017'
